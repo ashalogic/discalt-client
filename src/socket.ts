74b76680e0a06d236865ee90414c90f2f792be29
Parsiya Keshavarz <ashalogic@outlook.com>
2024-06-02 01:00:11 +0200

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -5,7 +5,7 @@ import { Manager } from "socket.io-client";
 
 // const [room, setRoom] = useState("");
 
-const manager = new Manager("https://bit.ly/discaltserver", {
+const manager = new Manager("https://a32b-2a02-8070-888-b3a0-f1c4-bb9f-b45b-b2c3.ngrok-free.app/", {
   autoConnect: false,
 });
 
