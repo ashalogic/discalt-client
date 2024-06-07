0fddc04d0e1f42cb675a8063b4e9742cc843cb03
Parsiya Keshavarz <ashalogic@outlook.com>
2024-06-07 14:47:46 +0200

    Update p2p.js

--- src/p2p.js
+++ src/p2p.js
@@ -30,27 +30,7 @@ export default class P2P extends EventEmitter {
     this.configuration = {
       iceServers: [
         {
-          urls: [
-            "stun:stun.l.google.com:19302",
-            "stun:stun1.l.google.com:19302",
-            "stun:stun2.l.google.com:19302",
-            "stun:stun3.l.google.com:19302",
-            "stun:stun4.l.google.com:19302",
-            "stun:stun01.sipphone.com",
-            "stun:stun.ekiga.net",
-            "stun:stun.fwdnet.net",
-            "stun:stun.ideasip.com",
-            "stun:stun.iptel.org",
-            "stun:stun.rixtelecom.se",
-            "stun:stun.schlund.de",
-            "stun:stunserver.org",
-            "stun:stun.softjoys.com",
-            "stun:stun.voiparound.com",
-            "stun:stun.voipbuster.com",
-            "stun:stun.voipstunt.com",
-            "stun:stun.voxgratia.org",
-            "stun:stun.xten.com",
-          ],
+          urls: ["stun:stun4.l.google.com:19302"],
         },
       ],
       iceCandidatePoolSize: 10,
